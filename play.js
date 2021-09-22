ef670411dbdd4f5eb60a90a737d75d175cec6e19
Russell Engebretson <russell.engebretson@gmail.com>
2021-09-22 20:53:46 +0000

    Refactored to put input in its own file

--- play.js
+++ play.js
@@ -1,24 +1,9 @@
 const net = require("net");
 const { connect } = require('./client');
-
-const setupInput = function(callback) {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  
-  stdin.on('data', callback);
-  return stdin;
-};
-
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
+const { setupInput } = require('./input');
 
 // establishes a connection with the game server
 console.log("Connecting ...");
 const conn = connect();
-setupInput(handleUserInput);
+let input = setupInput();
 
